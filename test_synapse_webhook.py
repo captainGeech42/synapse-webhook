efb7bbad1a850ba74136060266ae81e9517dfe6f
EXC3L-ONE <48698766+EXC3L-ONE@users.noreply.github.com>
2023-05-11 12:05:14 +0200

    Add support for Keybase webhooks

--- test_synapse_webhook.py
+++ test_synapse_webhook.py
@@ -96,6 +96,30 @@ class TeamsApiMock(s_httpapi.Handler):
                 return
 
         raise ValueError("bad teams webhook")
+    
+class KeybaseApiMock(s_httpapi.Handler):
+    """Mock implementation of a Keybase webhook server."""
+
+    counter = 0
+
+    async def head(self):
+        raise NotImplementedError
+
+    async def get(self):
+        raise NotImplementedError
+
+    async def post(self):
+        if self.request.body:
+            d = json.loads(self.request.body.decode())
+            m = d.get("msg")
+            if m is not None:
+                check_defang(m)
+                
+                KeybaseApiMock.counter += 1
+                self.set_status(200)
+                return
+
+        raise ValueError("bad keybase webhook")
 
 class SynapseWebhookTest(s_test.SynTest):
     async def _t_install_pkg(self, core: s_cortex.Cortex, prox: s_cortex.CoreApi | None = None):
@@ -120,10 +144,11 @@ class SynapseWebhookTest(s_test.SynTest):
             self.stormIsInPrint("Webhook added for service \"discord\"", await core.stormlist("zw.webhook.add asdf1 https://discord.com/api/webhooks/1111223423432/asdflkjsadlfkjadslfjlkfdsa-lkjczlknvznnmvxzc-oireoiwqrwoiuqroiweiuqorq"))
             self.stormIsInPrint("Webhook added for service \"slack\"", await core.stormlist("zw.webhook.add asdf2 https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"))
             self.stormIsInPrint("Webhook added for service \"teams\"", await core.stormlist("zw.webhook.add asdf3 https://asdfsafda.webhook.office.com/asdfasdfsafdsa/zxcvxzvxzvcxv/gdgfdsgfdsgfdg"))
+            self.stormIsInPrint("Webhook added for service \"keybase\"", await core.stormlist("zw.webhook.add asdf4 https://bots.keybase.io/webhookbot/asdasdasdasda_asdasdasdasda"))
             self.stormIsInErr("Failed to detect webhook service", await core.stormlist("zw.webhook.add asdf3 https://reddit.com"))
-            self.stormIsInErr("Invalid service specified: zzz", await core.stormlist("zw.webhook.add --service zzz asdf4 https://reddit.com"))
-            self.stormIsInPrint("Webhook added for service \"teams\"", await core.stormlist("zw.webhook.add --service teams asdf4 https://reddit.com"))
-            self.stormIsInErr("A webhook already exists", await core.stormlist("zw.webhook.add --service teams asdf4 https://reddit.com"))
+            self.stormIsInErr("Invalid service specified: zzz", await core.stormlist("zw.webhook.add --service zzz asdf5 https://reddit.com"))
+            self.stormIsInPrint("Webhook added for service \"teams\"", await core.stormlist("zw.webhook.add --service teams asdf5 https://reddit.com"))
+            self.stormIsInErr("A webhook already exists", await core.stormlist("zw.webhook.add --service teams asdf5 https://reddit.com"))
 
     async def test_call(self):
         async with self.getTestCoreAndProxy() as (core, prox):
@@ -137,10 +162,12 @@ class SynapseWebhookTest(s_test.SynTest):
             core.addHttpApi("/discord", DiscordApiMock, {"cell": core})
             core.addHttpApi("/slack", SlackApiMock, {"cell": core})
             core.addHttpApi("/teams", TeamsApiMock, {"cell": core})
+            core.addHttpApi("/keybase", KeybaseApiMock, {"cell": core})
 
             await core.callStorm(f"zw.webhook.add --service discord d1 https://root:root@127.0.0.1:{port}/discord")
             await core.callStorm(f"zw.webhook.add --service slack --global s1 https://root:root@127.0.0.1:{port}/slack")
             await core.callStorm(f"zw.webhook.add --service teams t1 https://root:root@127.0.0.1:{port}/teams")
+            await core.callStorm(f"zw.webhook.add --service teams k1 https://root:root@127.0.0.1:{port}/keybase")
 
             self.stormHasNoWarnErr(await core.stormlist("zw.webhook.send --no-verify d1 hello"))
             self.eq(DiscordApiMock.counter, 1)
@@ -163,6 +190,13 @@ class SynapseWebhookTest(s_test.SynTest):
             self.stormHasNoWarnErr(await core.stormlist("[inet:url=http://google.com inet:url=http://bing.com] | zw.webhook.send --no-verify --defang t1 `defang {$node.value()}`"))
             self.eq(TeamsApiMock.counter, 5)
 
+            self.stormHasNoWarnErr(await core.stormlist("zw.webhook.send --no-verify k1 hello"))
+            self.eq(KeybaseApiMock.counter, 1)
+            self.stormHasNoWarnErr(await core.stormlist("[inet:url=http://google.com inet:url=http://bing.com] | zw.webhook.send --no-verify k1 `{$node.value()}`"))
+            self.eq(KeybaseApiMock.counter, 3)
+            self.stormHasNoWarnErr(await core.stormlist("[inet:url=http://google.com inet:url=http://bing.com] | zw.webhook.send --no-verify --defang k1 `defang {$node.value()}`"))
+            self.eq(KeybaseApiMock.counter, 5)
+
             async with core.getLocalProxy(user="nopriv") as noprivcore:
                 self.stormIsInErr("Not authorized to use", [m async for m in noprivcore.storm("zw.webhook.send --no-verify d1 yeet")])
                 self.stormHasNoWarnErr([m async for m in noprivcore.storm("zw.webhook.send --no-verify s1 yeet")])
@@ -178,12 +212,14 @@ class SynapseWebhookTest(s_test.SynTest):
             self.stormIsInPrint("Webhook added for service \"discord\"", await core.stormlist("zw.webhook.add asdf1 https://discord.com/api/webhooks/1111223423432/asdflkjsadlfkjadslfjlkfdsa-lkjczlknvznnmvxzc-oireoiwqrwoiuqroiweiuqorq"))
             self.stormIsInPrint("Webhook added for service \"slack\"", await core.stormlist("zw.webhook.add asdf2 https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"))
             self.stormIsInPrint("Webhook added for service \"teams\"", await core.stormlist("zw.webhook.add asdf3 https://asdfsafda.webhook.office.com/asdfasdfsafdsa/zxcvxzvxzvcxv/gdgfdsgfdsgfdg"))
+            self.stormIsInPrint("Webhook added for service \"keybase\"", await core.stormlist("zw.webhook.add asdf4 https://bots.keybase.io/webhookbot/asdasdasdasda_asdasdasdasda"))
 
             mesgs = await core.stormlist("zw.webhook.list")
             self.stormHasNoWarnErr(mesgs)
             self.stormIsInPrint("Webhook: asdf1", mesgs)
             self.stormIsInPrint("Webhook: asdf2", mesgs)
             self.stormIsInPrint("Webhook: asdf3", mesgs)
+            self.stormIsInPrint("Webhook: asdf4", mesgs)
             
             async with core.getLocalProxy(user="nopriv") as noprivcore:
                 self.stormIsInErr("Not allowed to list", [m async for m in noprivcore.storm("zw.webhook.list --all")])
@@ -198,6 +234,7 @@ class SynapseWebhookTest(s_test.SynTest):
             self.stormIsInPrint("Webhook added for service \"discord\"", await core.stormlist("zw.webhook.add asdf1 https://discord.com/api/webhooks/1111223423432/asdflkjsadlfkjadslfjlkfdsa-lkjczlknvznnmvxzc-oireoiwqrwoiuqroiweiuqorq"))
             self.stormIsInPrint("Webhook added for service \"slack\"", await core.stormlist("zw.webhook.add asdf2 https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"))
             self.stormIsInPrint("Webhook added for service \"teams\"", await core.stormlist("zw.webhook.add asdf3 https://asdfsafda.webhook.office.com/asdfasdfsafdsa/zxcvxzvxzvcxv/gdgfdsgfdsgfdg"))
+            self.stormIsInPrint("Webhook added for service \"keybase\"", await core.stormlist("zw.webhook.add asdf4 https://bots.keybase.io/webhookbot/asdasdasdasda_asdasdasdasda"))
 
             self.stormIsInPrint("Webhook deleted: asdf1", await core.stormlist("zw.webhook.delete asdf1"))
             
